5c954d401d9b50b6dba217883cc95a8868f9ce07
Samra Husremović <samra.husremovic@gmail.com>
2011-10-22 14:16:00 +0200

    ver 2
    if-else statement in C, u C-u nema native string tipa varijabli pa smo
    koristili integer u samra_babo radi jednostavnosti.
    
    Mapirali smo tu varijablu i rekli: samra_babo = 1 radi se o Ernad
    Husremovic, samra_babo neki drugi broj = neki drugi babo

--- hello.c
+++ hello.c
@@ -4,8 +4,20 @@
    napisao hernad@bring.out.ba svojoj sceri Samri
 */
 
+/* legend: samrin_babo = 1 => Ernad
+           samrin_babo = 2 => other 
+*/
+
+const int samrin_babo = 2;
+
 int main()
 {
-  printf("hello world\n");
+  if (samrin_babo == 1)
+    printf("hello from Samra Husremovic");
+  else
+    printf("hello from neka druga Samra");
+
+  printf("\n");
+
   return 0;
 }
